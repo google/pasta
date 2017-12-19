4b750cb6cbd5cae446b1afdcd3285b7b588308bf
Nick Smith <smithnick@google.com>
2017-12-19 12:15:14 -0800

    Add golden tests for annotate to check that the prefix + suffix do not change
    This should catch regressions where the prefix or suffix is put on the
    wrong node. It's necessary to separate by python version because 2/3
    have different syntax trees. Also restructured the test classes with a
    metaclass style which seems a bit cleaner.

--- pasta/base/annotate_test.py
+++ pasta/base/annotate_test.py
@@ -21,6 +21,8 @@ from __future__ import print_function
 import ast
 import difflib
 import os.path
+from six import with_metaclass
+import sys
 import textwrap
 import unittest
 
@@ -34,7 +36,7 @@ TESTDATA_DIR = os.path.realpath(
     os.path.join(os.path.dirname(pasta.__file__), '../testdata'))
 
 
-class SymmetricTest(test_utils.TestCase):
+class PrefixSuffixTest(test_utils.TestCase):
 
   def test_block_suffix(self):
     src_tpl = textwrap.dedent('''\
@@ -84,7 +86,7 @@ class SymmetricTest(test_utils.TestCase):
       self.assertMultiLineEqual(
           expected, actual,
           'Incorrect suffix for code:\n%s\nNode: %s (line %d)\nDiff:\n%s' % (
-              src, node, node.lineno, '\n'.join(get_diff(actual, expected))))
+              src, node, node.lineno, '\n'.join(_get_diff(actual, expected))))
 
   def test_no_block_suffix_for_single_line_statement(self):
     src = 'if x:  return y\n  #a\n#b\n'
@@ -104,26 +106,6 @@ class SymmetricTest(test_utils.TestCase):
     self.assertEqual('', ast_utils.prop(t.body[1], 'suffix'))
 
 
-def symmetric_test_generator(filepath):
-  def test(self):
-    with open(filepath, 'r') as handle:
-      src = handle.read()
-
-    t = ast_utils.parse(src)
-    annotator = annotate.AstAnnotator(src)
-    annotator.visit(t)
-
-    output = codegen.to_str(t)
-
-    self.assertEqual([], annotator.tokens._parens, 'Unmatched parens')
-    self.assertMultiLineEqual(output, src)
-  return test
-
-
-def get_diff(before, after):
-  return difflib.ndiff(after.splitlines(), before.splitlines())
-
-
 def _is_syntax_valid(filepath):
   with open(filepath, 'r') as f:
     try:
@@ -133,20 +115,133 @@ def _is_syntax_valid(filepath):
   return True
 
 
-data_dir = os.path.join(TESTDATA_DIR, 'ast')
-for dirpath, dirs, files in os.walk(data_dir):
-  for filename in files:
-    if filename.endswith('.in'):
-      full_path = os.path.join(dirpath, filename)
-      setattr(SymmetricTest, 'test_symmetric_' + filename[:-3], unittest.skipIf(
-                not _is_syntax_valid(full_path),
-                'Test contains syntax not supported by this version.',
-              )(symmetric_test_generator(full_path)))
+class SymmetricTestMeta(type):
+
+  def __new__(mcs, name, bases, inst_dict):
+    # Helper function to generate a test method
+    def symmetric_test_generator(filepath):
+      def test(self):
+        with open(filepath, 'r') as handle:
+          src = handle.read()
+        t = ast_utils.parse(src)
+        annotator = annotate.AstAnnotator(src)
+        annotator.visit(t)
+        self.assertMultiLineEqual(codegen.to_str(t), src)
+        self.assertEqual([], annotator.tokens._parens, 'Unmatched parens')
+      return test
+
+    # Add a test method for each input file
+    test_method_prefix = 'test_symmetric_'
+    data_dir = os.path.join(TESTDATA_DIR, 'ast')
+    for dirpath, dirs, files in os.walk(data_dir):
+      for filename in files:
+        if filename.endswith('.in'):
+          full_path = os.path.join(dirpath, filename)
+          inst_dict[test_method_prefix + filename[:-3]] = unittest.skipIf(
+                    not _is_syntax_valid(full_path),
+                    'Test contains syntax not supported by this version.',
+                  )(symmetric_test_generator(full_path))
+    return type.__new__(mcs, name, bases, inst_dict)
+
+
+class SymmetricTest(with_metaclass(SymmetricTestMeta, test_utils.TestCase)):
+  """Validates the symmetry property.
+
+  After parsing + annotating a module, regenerating the source code for it
+  should yield the same result.
+  """
+
+
+def _get_node_identifier(node):
+  for attr in ('id', 'name', 'attr', 'arg', 'module'):
+    if isinstance(getattr(node, attr, None), str):
+      return getattr(node, attr, '')
+  return ''
+
+
+class PrefixSuffixGoldenTestMeta(type):
+
+  def __new__(mcs, name, bases, inst_dict):
+    # Helper function to generate a test method
+    def golden_test_generator(input_file, golden_file):
+      def test(self):
+        with open(input_file, 'r') as handle:
+          src = handle.read()
+        t = ast_utils.parse(src)
+        annotator = annotate.AstAnnotator(src)
+        annotator.visit(t)
+
+        def escape(s):
+          return '' if s is None else s.replace('\n', '\\n')
+
+        result = '\n'.join(
+            "{0:12} {1:20} \tprefix=|{2}|\tsuffix=|{3}|".format(
+                str((getattr(n, 'lineno', -1), getattr(n, 'col_offset', -1))),
+                type(n).__name__ + ' ' + _get_node_identifier(n),
+                escape(ast_utils.prop(n, 'prefix')),
+                escape(ast_utils.prop(n, 'suffix')))
+            for n in ast.walk(t)) + '\n'
+
+        # If specified, write the golden data instead of checking it
+        if getattr(self, 'generate_goldens', False):
+          if not os.path.isdir(os.path.dirname(golden_file)):
+            os.makedirs(os.path.dirname(golden_file))
+          with open(golden_file, 'w') as f:
+            f.write(result)
+          print('Wrote: ' + golden_file)
+          return
+
+        try:
+          with open(golden_file, 'r') as f:
+            golden = f.read()
+        except IOError:
+          self.fail('Missing golden data.')
+
+        self.assertMultiLineEqual(golden, result)
+      return test
+
+    # Add a test method for each input file
+    test_method_prefix = 'test_golden_prefix_suffix_'
+    data_dir = os.path.join(TESTDATA_DIR, 'ast')
+    python_version = '%d.%d' % sys.version_info[:2]
+    for dirpath, dirs, files in os.walk(data_dir):
+      for filename in files:
+        if filename.endswith('.in'):
+          full_path = os.path.join(dirpath, filename)
+          golden_path = os.path.join(dirpath, 'golden', python_version,
+                                     filename[:-3] + '.out')
+          inst_dict[test_method_prefix + filename[:-3]] = unittest.skipIf(
+                    not _is_syntax_valid(full_path),
+                    'Test contains syntax not supported by this version.',
+                  )(golden_test_generator(full_path, golden_path))
+    return type.__new__(mcs, name, bases, inst_dict)
+
+
+class PrefixSuffixGoldenTest(with_metaclass(PrefixSuffixGoldenTestMeta,
+                                            test_utils.TestCase)):
+  """Checks the prefix and suffix on each node in the AST.
+
+  This uses golden files in testdata/ast/golden. To regenerate these files, run
+  python setup.py test -s pasta.base.annotate_test.generate_goldens
+  """
+
+
+def _get_diff(before, after):
+  return difflib.ndiff(after.splitlines(), before.splitlines())
 
 
 def suite():
   result = unittest.TestSuite()
   result.addTests(unittest.makeSuite(SymmetricTest))
+  result.addTests(unittest.makeSuite(PrefixSuffixTest))
+  result.addTests(unittest.makeSuite(PrefixSuffixGoldenTest))
+  return result
+
+
+def generate_goldens():
+  result = unittest.TestSuite()
+  result.addTests(unittest.makeSuite(PrefixSuffixGoldenTest))
+  setattr(PrefixSuffixGoldenTest, 'generate_goldens', True)
   return result
 
 
