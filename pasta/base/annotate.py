947013811be47f2a6ab14ea4b654c8b4cc2631d7
William Jamir <williamjamir@gmail.com>
2018-01-08 22:54:29 -0200

    Use scope to handle parentheses in import statement

--- pasta/base/annotate.py
+++ pasta/base/annotate.py
@@ -545,12 +545,12 @@ class BaseVisitor(ast.NodeVisitor):
     self.attr(node, 'module_suffix', [self.ws], default=' ')
 
     self.token('import')
-    self.optional_token(node, 'open_imports', '(')
-    for alias in node.names:
-      self.visit(alias)
-      if alias != node.names[-1]:
-        self.token(',')
-    self.optional_token(node, 'close_imports', ')')
+    with self.scope(node, 'names'):
+      for alias in node.names:
+        self.visit(alias)
+        if alias != node.names[-1]:
+          self.token(',')
+
 
   @statement
   def visit_Nonlocal(self, node):
