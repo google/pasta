f995fc5ab095b4390fddf32eec12136bf7ce5289
Nick Smith <smithnick@google.com>
2018-02-16 14:57:40 -0800

    Don't compare numbers to zero, in order to support complex numbers

--- pasta/base/annotate.py
+++ pasta/base/annotate.py
@@ -1058,7 +1058,7 @@ class AstAnnotator(BaseVisitor):
     """Annotate a Num node with the exact number format."""
     token_number_type = token_generator.TOKENS.NUMBER
     contentargs = [lambda: self.tokens.next_of_type(token_number_type).src]
-    if node.n < 0 or node.n == 0 and self.tokens.peek().src == '-':
+    if self.tokens.peek().src == '-':
       contentargs.insert(0, '-')
     self.attr(node, 'content', contentargs, deps=('n',), default=str(node.n))
 
