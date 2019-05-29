f26107d1f27848ab67387c7f4697de2393b3e4d9
Nick Smith <smithnick@google.com>
2019-05-29 10:04:48 -0700

    Bugfix for fstring FormattedValues that begin with a paren
    The '(' was being incorrectly attributed to the FormattedValue node
    rather than its value. FormattedValue nodes don't have extra parentheses
    beyond the expression they contain.

--- pasta/base/annotate.py
+++ pasta/base/annotate.py
@@ -63,7 +63,7 @@ def expression(f):
 
 def fstring_expression(f):
   """Decorates a function where the node is a FormattedValue in an fstring."""
-  return _gen_wrapper(f)
+  return _gen_wrapper(f, scope=False)
 
 
 def space_around(f):
